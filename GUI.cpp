3b3c8cb346e9076ef580a34b91b5be64496b0d30
tddixon0 <119270651+tddixon0@users.noreply.github.com>
2023-12-04 16:53:26 -0500

    Create GUI.cpp

---
+++ GUI.cpp
@@ -0,0 +1,38 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    // Create a main window
+    QWidget mainWindow;
+    mainWindow.setWindowTitle("Simple GUI Example");
+
+    // Create a layout for the main window
+    QVBoxLayout layout(&mainWindow);
+
+    // Create a label
+    QLabel label("Hello, GUI World!", &mainWindow);
+    label.setAlignment(Qt::AlignCenter);
+
+    // Create a button
+    QPushButton button("Click Me", &mainWindow);
+
+    // Connect a slot (function) to the button's click signal
+    QObject::connect(&button, &QPushButton::clicked, [&label]() {
+        label.setText("Button Clicked!");
+    });
+
+    // Add the label and button to the layout
+    layout.addWidget(&label);
+    layout.addWidget(&button);
+
+    // Show the main window
+    mainWindow.show();
+
+    // Start the application event loop
+    return app.exec();
+}
