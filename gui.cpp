f3a6994cb122e280bee257e2605d94ce56b4ed6c
tddixon0 <119270651+tddixon0@users.noreply.github.com>
2023-12-05 20:55:18 -0500

    Staging

--- gui.cpp
+++ gui.cpp
@@ -5,7 +5,7 @@
 #include <QtWidgets/QVBoxLayout>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    QApplication gui(argc, argv);
 
     // Create a main window
     QWidget mainWindow;
@@ -34,5 +34,5 @@ int main(int argc, char *argv[]) {
     mainWindow.show();
 
     // Start the application event loop
-    return app.exec();
+    return gui.exec();
 }
