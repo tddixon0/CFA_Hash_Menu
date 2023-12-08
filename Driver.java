b276c98451cd0c430de3179d115b8f78897faa23
vonhagelethan <127246145+vonhagelethan@users.noreply.github.com>
2023-12-08 00:59:36 -0500

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 public class Driver {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
+        String name;
+        String choice;
         System.out.println("Welcome to CFA! What would you like to do?");
-        if (input.next() == help) {
-
+        choice = input.nextLine();
+        if (choice.equals("help")) {
+            JDBC.main();
+            System.out.println("What would you like to do?");
+            choice = input.nextLine();
+        }
+        if (choice.equals("order")) {
+            System.out.println("Can I have a name for the order?");
+            name = input.nextLine();
+            System.out.println("Okay " + name + ", what can I get for you?");
+            
         }
     }
 }
